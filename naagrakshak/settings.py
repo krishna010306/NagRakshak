dc1b90e8be263fcc60f43c5c8726a1b9496785da
krishna <zurmurekrishna25@gmail.com>
2026-03-29 07:36:38 +0530

    fix mysqlclient version

--- naagrakshak/settings.py
+++ naagrakshak/settings.py
@@ -84,12 +84,8 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQLDATABASE'),
-        'USER': os.environ.get('MYSQLUSER'),
-        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
-        'HOST': os.environ.get('MYSQLHOST'),
-        'PORT': os.environ.get('MYSQLPORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
